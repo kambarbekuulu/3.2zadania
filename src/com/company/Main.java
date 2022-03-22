a4562ea01830a095f8f44f091f44c097d1e5af17
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-03-22 15:38:31 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s  ;
+        System.out.println("tort orundu san jazynyz:  " );
+        s = sc.nextLine();
+        System.out.println(s.charAt(0)+","+s.charAt(1)+","+s.charAt(2)+","+s.charAt(3));
+
+    }
+}
